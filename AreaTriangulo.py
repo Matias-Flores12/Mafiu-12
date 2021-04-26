0f585b4295e1bef84c49321fd803bb0d070c6b51
MatiasFlores12 <>
2021-04-26 19:33:27 +0000

    Ejemplo en 8 lenguages

---
+++ AreaTriangulo.py
@@ -0,0 +1,9 @@
+#Variables de entrada y otros
+print("Ejercicio 01: Area Triangulo")
+#Datos de entrada ingresado por dispositivos de entrada
+b=int(input("Ingrese base"))
+a=int(input("Ingrese altura"))
+#Proceso
+area=(b*h)/2
+#Datos de salida
+print("El area del Triangulo es:" ,area)
---
+++ index.py
@@ -0,0 +1,7 @@
+print("Hello my dear Mati")
+a=60
+b=5
+print("Suma",a+b)
+print("Resta:",a-b)
+print("Division:",a/b)
+print("Multiplicacion:",a*b)
